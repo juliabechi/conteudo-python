086ff2b7cb2eb680120af2c37510b8c281d09c92
Aluno SENAI <Aluno@SN-345388.edu.fiesc.com.br>
2025-10-03 16:59:02 -0300

    exercicios

---
+++ exercicios/exercicio_3.py
@@ -0,0 +1,3 @@
+numero_1 = int(input("Digite o valor 1: "))
+numero_2 = int(input("Digite o valor 2: "))
+
---
+++ exercicios/exercicio_4.py
@@ -0,0 +1,10 @@
+nota_1 = float("Digite a primeira nota: "))
+nota_2 = float("Digite a segunda nota: "))
+nota_3 = float("Digite a terceira nota: "))
+nota_4 = float("Digite a quarta nota: "))
+
+soma = nota_1 + nota_2 + nota_3 + nota_4
+media = soma / 4
+media_outro = (nota_1 + nota_2 + nota_3 + nota_4) / 4
+
+print("A média do aluno é: ", media)
