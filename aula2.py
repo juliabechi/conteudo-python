82dd831cebbdad70750e434088a54650d637b719
Aluno SENAI <Aluno@SN-345388.edu.fiesc.com.br>
2025-10-03 16:58:50 -0300

    Aula2: variaveis, input e calculos

---
+++ aula2.py
@@ -0,0 +1,25 @@
+# Para criar uma variavel, colocamos:
+# o nome dela
+# o simbolo de atribuição (=)
+# o valor inicial dela
+
+# Uma variavel chamada nome que recebe o meu nome
+nome = "Julia"
+print("Olá, me chamo",nome)
+print("O tipo de variavelé",type(nome))
+print()
+# Uma variavel do tipo inteiro
+quantidade_alunos = 16
+print("Estamos em",quantidade_alunos,"alunos na sala")
+print("o tipo de variavel é",type(quantidade_alunos))
+print()
+# uma variavel do tipo ...
+salario_bruto = 1425.23
+print("O salário mínimo é R$",salario_bruto)
+print("O tipo de variavel é",type(salario_bruto))
+print()
+# Uma variavel do tipo ...
+is_aberta = False
+print("a porta está aberta?",type(is_aberta))
+print("O tipo de variavel é",type(is_aberta))
+
---
+++ aula2_calculos.py
@@ -0,0 +1,28 @@
+# Arquivo para aprende a realizar cálculos com Python
+
+# Sinais matemáticos:
+# + Soma              Serve para somar valores
+# - Subtração         Serve para diminuir um valor do outro
+# * Multiplicação     Serve para multiplicar valores
+# / Divisão           Serve para dividir um valor do outro
+# ** Potência         Serve para fazer a potência de valores
+# // Divisão inteira        Serve para retornar a parte inteira da divisão
+# % resto de divisão        Serve para retornar o resto da divisão
+
+numero_1 = int(input("Digite o primeiro valor: "))
+numero_2 = int(input("Digite o segundo valor: "))
+
+resultado = numero_1 + numero_2
+print("A subtração do numero 1 com o numero 2 é:",resultado)
+
+resultado = numero_1 - numero_2
+print("A subtração do numero 1 pelo numero 2 é: ",resultado)
+
+resultado = numero_1 * numero_2
+print("A multiplicação do numero 1 pelo numero 2 é: ",resultado)
+
+resultado = numero_1 / numero_2
+
+print("A divisão do numero 1 pelo numero 2 é: ",resultado)
+
+resultado= numero_1 
